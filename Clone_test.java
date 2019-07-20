a101a5d80bd30e0af45c3a1ea3995dda1002592f
ybx <401129874@qq.com>
2019-07-21 02:02:01 +0800

    浅克隆及其测试

---
+++ Clone_test.java
@@ -0,0 +1,16 @@
+public class Clone_test {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Address address = new Address("No.23");
+        Stu stu = new Stu("张三","12",address);
+        Object clone = stu.clone();
+        Stu stu1 = (Stu)clone;
+        System.out.println(stu1.getAddress().getId() + " " + stu1.getName() + " " + stu1.getAge());
+        Address address1 = new Address("No.99");
+        address.setId("No.99");
+        stu1.setAddress(address);
+        stu1.setName("李四");
+        stu1.setAge("14");
+        System.out.println(stu.getAddress().getId() + " " + stu.getName() + " " + stu.getAge());
+        System.out.println(stu1.getAddress().getId() + " " + stu1.getName() + " " + stu1.getAge());
+    }
+}
